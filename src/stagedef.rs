6323b455bdd4914fdb505e65a4d76d7b2db75af3
The BombSquad <35253266+TheBombSquad@users.noreply.github.com>
2022-05-31 04:06:39 -0400

    Started adding stagedef struct definitions

--- src/stagedef.rs
+++ src/stagedef.rs
@@ -1,6 +1,7 @@
 use std::{fs, collections};
 use std::path::PathBuf;
 
+// Instance-related stuff
 pub struct StageDefInstance {
     pub stagedef: StageDef,
     pub file_path: PathBuf,
@@ -34,6 +35,8 @@ impl StageDefInstance {
         })
     }
 }
+
+// Common structures/enums
 pub struct Vector3 {
     pub x: f32,
     pub y: f32,
@@ -57,15 +60,152 @@ pub enum Endianness {
     LittleEndian,
 }
 
+pub enum AnimationState {
+    Play,
+    Pause,
+    Reverse,
+    FastForward,
+    FastReverse,
+}
+
+pub enum AnimationType {
+    LoopingAnimation,
+    PlayOnceAnimation,
+    Seesaw
+}
+
+pub enum GoalType {
+    Blue,
+    Green,
+    Red,
+}
+
+pub enum BananaType {
+    Single,
+    Bunch,
+}
+
+pub struct CollisionTriangle {
+    pub position: Vector3,
+    pub normal: Vector3,
+    pub rotation: ShortVector3,
+    pub delta_x2_x1: f32,
+    pub delta_y2_y1: f32,
+    pub delta_x3_x1: f32,
+    pub delta_y3_y1: f32,
+    pub x_tangent: f32,
+    pub y_tangent: f32,
+    pub x_bitangent: f32,
+    pub y_bitangent: f32,
+}
+
+pub struct Animation {
+}
+
+pub struct Goal {
+    pub position: Vector3,
+    pub rotation: ShortVector3,
+    pub goal_type: GoalType,
+} 
+
+pub struct Bumper {
+    pub position: Vector3,
+    pub rotation: ShortVector3,
+    pub scale: Vector3,
+}
+
+pub struct Jamabar {
+    pub position: Vector3,
+    pub rotation: ShortVector3,
+    pub scale: Vector3,
+}
+
+pub struct Banana {
+    pub position: Vector3,
+    pub banana_type: BananaType,
+}
+
+pub struct ConeCollisionObject {
+    pub position: Vector3,
+    pub rotation: ShortVector3,
+    pub radius_1: f32,
+    pub height: f32,
+    pub radius_2: f32,
+}
+
+pub struct SphereCollisionObject {
+    pub position: Vector3,
+    pub radius: f32,
+    pub unk0x10: u32,
+}
+
+pub struct CylinderCollisionObject {
+    pub position: Vector3,
+    pub radius: f32,
+    pub height: f32,
+    pub rotation: ShortVector3,
+    pub unk0x1a: u16,
+}
+
+pub struct FalloutVolume {
+    pub position: Vector3,
+    pub size: Vector3,
+    pub rotation: ShortVector3,
+    pub unk0x1e: u16,
+}
+
+
+pub struct CollisionHeader {
+    pub center_of_rotation_position: f32,
+    pub conveyor_vector: f32,
+
+    pub collision_triangles: Vec<CollisionTriangle>,
+    pub collision_grid_start_x: f32,
+    pub collision_grid_start_z: f32,
+    pub collision_grid_step_size_x: f32,
+    pub collision_grid_step_size_z: f32,
+    pub collision_grid_step_count_x: u32,
+    pub collision_grid_step_count_z: u32,
+    
+    pub seesaw_sensitivity: f32,
+    pub seesaw_friction: f32,
+    pub seesaw_spring: f32,
+
+    pub animation_loop_point: f32,
+    pub animation_state_init: AnimationState,
+    pub animation_type: AnimationType, 
+    pub animation_id: u16,
+
+    pub unk0x9c: u32,
+    pub unk0xa0: u32,
+    pub unk0xb0: u32,
+    pub unk0xd0: u32,
+    pub unk0xa6: u16,
+
+    pub goals: Vec<Box<Goal>>,
+    pub bumpers: Vec<&Bumper>,
+    pub jamabars: Vec<&Jamabar>,
+    pub bananas: Vec<&Banana>,
+    pub cone_collision_objects: Vec<&ConeCollisionObject>,
+    pub sphere_collision_objects: Vec<&SphereCollisionObject>,
+    pub cylinder_collision_objects: Vec<&CylinderCollisionObject>,
+    pub fallout_volumes: Vec<&FalloutVolume>,
+
+}
+
 pub struct StageDef {
-    test: i32,
+    pub magic_number_1: f32,
+    pub magic_number_2: f32,
+
+    pub start_position: Vector3,
+    pub start_rotation: ShortVector3,
+    
+    pub collision_headers: Vec<CollisionHeader>,
+
 }
 
 impl StageDef {
     fn new(bin: Vec<u8>, game: &Game, endianness: &Endianness) -> Result<Self, std::io::Error>{
-        let test = 3;
-        Ok(Self {
-            test
-        })
+        Ok(Self{})
     }
 }
